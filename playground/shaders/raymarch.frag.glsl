ee96d7c71f7c05f6c9bc18d5012cec2706b30d82
hadron13 <carlosbpfonseca@gmail.com>
2025-09-18 19:39:36 -0300

    shadows

--- playground/shaders/raymarch.frag.glsl
+++ playground/shaders/raymarch.frag.glsl
@@ -53,8 +53,8 @@ float difference (float a, float b){
 }
 
 float map(vec3 position){
-    position = repeated(position, vec3(3.0));
-    float dist = sphere(vec3(0, 0, 1.5), 0.4, position);
+    float dist = sphere(vec3(-0.3, 0, 0.5), 0.25, position);
+    dist = join (dist, sphere(vec3(0.3, 0, 0.5), 0.25, position));
     return dist;
 }
 
@@ -67,19 +67,29 @@ vec3 normal(vec3 position){
     ));
 }
 
+float shadow(vec3 position, vec3 to_light_direction, float min_t, float max_t){
+    float t = min_t;
+    for(int i = 0; i < 512 && t < max_t; i++){
+        float dist = map(position + to_light_direction * t);
+        if(dist < 0.001){
+            return 0.1;
+        }
+        t += dist;
+    }
+    return 1.0;
+}
+
 void main(){
 
     vec2 uv = gl_FragCoord.xy/resolution.y - vec2((resolution.x/resolution.y - 1.0)/2.0, 0);
     vec2 centered_uv = (uv - 0.5)*2;
 
     // vec3 ray_position = vec3(sin(time), 0, cos(time) + 0.75) * 2;
-    vec3 ray_position = vec3(0);
+    vec3 ray_position = vec3(0, 0, -0.4);
     mat4 view = viewMatrix(ray_position, vec3(0, 0, -1.0), vec3(0, 1.0, 0));
     
     vec3 ray_direction = normalize(vec3(centered_uv, 1.0));
     ray_direction = (view * vec4(ray_direction, 1.0)).xyz;
-
-    vec3 sphere_origin = vec3(0, 0, 2.0);
 
     vec3 color = vec3(0);
 
@@ -92,20 +102,22 @@ void main(){
 
             vec3 obj_color = vec3(0.86, 0.61, 0.10);
             vec3 light_color = vec3(1.0);
-            vec3 light_position = vec3(1.0, 1.0, 0.0);
+            vec3 light_position = vec3(2.0, 1.0, 0.5);
+
 
             vec3 light_direction = normalize(light_position - ray_position);
             vec3 reflect_direction = reflect(-light_direction, normal);  
 
             float light_distance = length(light_position - ray_position);
-            float attenuation = 1.0 / (0.0 + 0.5 * light_distance + 0.3 * light_distance * light_distance); 
+            float attenuation = 1.0 / (0.1 + 0.5 * light_distance + 0.05 * light_distance * light_distance); 
             
-            vec3 ambient = 0.1 * light_color * attenuation;
-            vec3 diffuse = (max(dot(normal, light_direction), 0)) * light_color * attenuation;
-            vec3 specular = pow(max(dot(-ray_direction, reflect_direction), 0.0), 16) * 0.5 * light_color * attenuation;
+            vec3 ambient = 0.1 * light_color;
+            vec3 diffuse = (max(dot(normal, light_direction), 0)) * light_color;
+            vec3 specular = pow(max(dot(-ray_direction, reflect_direction), 0.0), 16) * 0.5 * light_color;
 
+            float shadow = shadow(ray_position, light_direction, 0.01, light_distance);
 
-            color = (ambient + diffuse + specular) * obj_color;
+            color = (ambient + diffuse + specular) * attenuation * obj_color * shadow;
 
             break;
         }
